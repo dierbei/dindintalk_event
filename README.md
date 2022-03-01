2e7c9b0821ab5c152ef636f7b348df446741fc55
xiaolatiao <1628652790@qq.com>
2022-03-01 19:19:20 +0800

    feat：钉钉事件订阅

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # dindintalk_event
 钉钉事件订阅-Golang版本
+参考：https://github.com/open-dingtalk/dingtalk-callback-Crypto?spm=ding_open_doc.document.0.0.633c722fb7BrRp
---
+++ main.go
@@ -0,0 +1,239 @@
+package main
+
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha1"
+	"encoding/base64"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"log"
+	r "math/rand"
+	"net/http"
+	"os"
+	"sort"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type DingTalkCrypto struct {
+	Token          string
+	EncodingAESKey string
+	SuiteKey       string
+	BKey           []byte
+	Block          cipher.Block
+}
+
+type EncryptData struct {
+	Encrypt string `json:"encrypt"`
+}
+
+func NewEncryptData() *EncryptData {
+	return &EncryptData{}
+}
+
+func main() {
+	r := gin.Default()
+
+	// 钉钉事件订阅回调
+	r.POST("/getCallBalk", GetCallBalk)
+
+	// 测试方法
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "success",
+		})
+	})
+
+	if err := r.Run(":8083"); err != nil {
+		log.Fatalf("8083 启动失败")
+	}
+}
+
+func GetCallBalk(ctx *gin.Context) {
+	var (
+		msgSignature string       //消息体签名
+		timeStamp    string       //时间戳
+		nonce        string       //随机字符串
+		encrypt      *EncryptData //加密的推送事件
+	)
+
+	if msgSignature = ctx.Query("msg_signature"); msgSignature == "" {
+		msgSignature = ctx.Query("signature")
+	}
+	if timeStamp = ctx.Query("timeStamp"); timeStamp == "" {
+		timeStamp = ctx.Query("timestamp")
+	}
+	nonce = ctx.Query("nonce")
+	encrypt = NewEncryptData()
+	if err := ctx.ShouldBindJSON(encrypt); err != nil {
+		log.Println("绑定encrypt数据失败")
+	}
+
+	var ding = NewDingTalkCrypto(os.Getenv("DIN_TOKEN"), os.Getenv("DIN_AESKEY"), os.Getenv("DIN_APPKEY"))
+	msg, err := ding.GetEncryptMsg("success")
+	if err != nil {
+		log.Println("获取加密消息失败")
+	}
+	// 打印内容：map[encrypt:f2MzzKpRUm77eqd5liytSl2ai1yYtL26SPOftUMuvc9PB8Ra2flGZOcvuum1r9qP msg_signature:63c3c7c7156843725d2eb1199d32f2b8bc43b8bb nonce:g17xDE1v7v9g timeStamp:29]
+	fmt.Println(msg)
+
+	success, _ := ding.GetDecryptMsg(msgSignature, timeStamp, nonce, encrypt.Encrypt)
+	// 打印内容：【GetDecryptMsg】{"CorpId":"ding1b32d00deb9a609f24f2f5cc6abecb85","EventType":"user_modify_org","UserId":["0201385360829542"],"TimeStamp":"1646113889363"}
+	fmt.Println("【GetDecryptMsg】" + success)
+	ctx.JSON(http.StatusOK, msg)
+}
+
+func NewDingTalkCrypto(token, encodingAESKey, suiteKey string) *DingTalkCrypto {
+	//fmt.Println(len(encodingAESKey))
+	if len(encodingAESKey) != int(43) {
+		panic("不合法的EncodingAESKey")
+	}
+	bkey, err := base64.StdEncoding.DecodeString(encodingAESKey + "=")
+	if err != nil {
+		panic(err.Error())
+	}
+	block, err := aes.NewCipher(bkey)
+	if err != nil {
+		panic(err.Error())
+	}
+	c := &DingTalkCrypto{
+		Token:          token,
+		EncodingAESKey: encodingAESKey,
+		SuiteKey:       suiteKey,
+		BKey:           bkey,
+		Block:          block,
+	}
+	return c
+}
+
+func (c *DingTalkCrypto) GetDecryptMsg(signature, timestamp, nonce, secretMsg string) (string, error) {
+	if !c.VerificationSignature(c.Token, timestamp, nonce, secretMsg, signature) {
+		return "", errors.New("ERROR: 签名不匹配")
+	}
+	decode, err := base64.StdEncoding.DecodeString(secretMsg)
+	if err != nil {
+		return "", err
+	}
+	if len(decode) < aes.BlockSize {
+		return "", errors.New("ERROR: 密文太短")
+	}
+	blockMode := cipher.NewCBCDecrypter(c.Block, c.BKey[:c.Block.BlockSize()])
+	plantText := make([]byte, len(decode))
+	blockMode.CryptBlocks(plantText, decode)
+	plantText = pkCS7UnPadding(plantText)
+	size := binary.BigEndian.Uint32(plantText[16:20])
+	plantText = plantText[20:]
+	corpID := plantText[size:]
+	if string(corpID) != c.SuiteKey {
+		return "", errors.New("ERROR: CorpID匹配不正确")
+	}
+	return string(plantText[:size]), nil
+}
+
+func (c *DingTalkCrypto) GetEncryptMsg(msg string) (map[string]string, error) {
+	var timestamp = time.Now().Second()
+	var nonce = randomString(12)
+	str, sign, err := c.GetEncryptMsgDetail(msg, fmt.Sprint(timestamp), nonce)
+
+	return map[string]string{"nonce": nonce, "timeStamp": fmt.Sprint(timestamp), "encrypt": str, "msg_signature": sign}, err
+}
+
+func (c *DingTalkCrypto) GetEncryptMsgDetail(msg, timestamp, nonce string) (string, string, error) {
+	size := make([]byte, 4)
+	binary.BigEndian.PutUint32(size, uint32(len(msg)))
+	msg = randomString(16) + string(size) + msg + c.SuiteKey
+	plantText := pkCS7Padding([]byte(msg), c.Block.BlockSize())
+	if len(plantText)%aes.BlockSize != 0 {
+		return "", "", errors.New("ERROR: 消息体size不为16的倍数")
+	}
+	blockMode := cipher.NewCBCEncrypter(c.Block, c.BKey[:c.Block.BlockSize()])
+	chipherText := make([]byte, len(plantText))
+	blockMode.CryptBlocks(chipherText, plantText)
+	outMsg := base64.StdEncoding.EncodeToString(chipherText)
+	signature := c.CreateSignature(c.Token, timestamp, nonce, string(outMsg))
+	return string(outMsg), signature, nil
+}
+
+func sha1Sign(s string) string {
+	// The pattern for generating a hash is `sha1.New()`,
+	// `sha1.Write(bytes)`, then `sha1.Sum([]byte{})`.
+	// Here we start with a new hash.
+	h := sha1.New()
+
+	// `Write` expects bytes. If you have a string `s`,
+	// use `[]byte(s)` to coerce it to bytes.
+	h.Write([]byte(s))
+
+	// This gets the finalized hash result as a byte
+	// slice. The argument to `Sum` can be used to append
+	// to an existing byte slice: it usually isn't needed.
+	bs := h.Sum(nil)
+
+	// SHA1 values are often printed in hex, for example
+	// in git commits. Use the `%x` format verb to convert
+	// a hash results to a hex string.
+	return fmt.Sprintf("%x", bs)
+}
+
+// 数据签名
+func (c *DingTalkCrypto) CreateSignature(token, timestamp, nonce, msg string) string {
+	params := make([]string, 0)
+	params = append(params, token)
+	params = append(params, timestamp)
+	params = append(params, nonce)
+	params = append(params, msg)
+	sort.Strings(params)
+	return sha1Sign(strings.Join(params, ""))
+}
+
+// 验证数据签名
+func (c *DingTalkCrypto) VerificationSignature(token, timestamp, nonce, msg, sigture string) bool {
+	return c.CreateSignature(token, timestamp, nonce, msg) == sigture
+}
+
+// 解密补位
+func pkCS7UnPadding(plantText []byte) []byte {
+	length := len(plantText)
+	unpadding := int(plantText[length-1])
+	return plantText[:(length - unpadding)]
+}
+
+// 加密补位
+func pkCS7Padding(ciphertext []byte, blockSize int) []byte {
+	padding := blockSize - len(ciphertext)%blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(ciphertext, padtext...)
+}
+
+// 随机字符串
+func randomString(n int, alphabets ...byte) string {
+	const alphanum = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	var bytes = make([]byte, n)
+	var randby bool
+	if num, err := rand.Read(bytes); num != n || err != nil {
+		r.Seed(time.Now().UnixNano())
+		randby = true
+	}
+	for i, b := range bytes {
+		if len(alphabets) == 0 {
+			if randby {
+				bytes[i] = alphanum[r.Intn(len(alphanum))]
+			} else {
+				bytes[i] = alphanum[b%byte(len(alphanum))]
+			}
+		} else {
+			if randby {
+				bytes[i] = alphabets[r.Intn(len(alphabets))]
+			} else {
+				bytes[i] = alphabets[b%byte(len(alphabets))]
+			}
+		}
+	}
+	return string(bytes)
+}
