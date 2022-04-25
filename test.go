e3a336e7911f8f2c0ef4e97c1d88a3fd76aaecc7
和兑 <chenzhangchao@cai-inc.com>
2022-04-25 17:51:37 +0800

    feat:test..

--- test.go
+++ test.go
@@ -1,8 +1,26 @@
 package main
 
+import "strings"
+
 //
 ///
 //
 /*
 
  */
+
+func isAddComment(line string) bool {
+	if strings.HasPrefix(line, "+") {
+		if strings.HasPrefix(strings.TrimSpace(line[1:]), "//") ||
+			strings.HasPrefix(strings.TrimSpace(line[1:]), "/*") ||
+			strings.HasPrefix(strings.TrimSpace(line[1:]), "///") ||
+			strings.HasPrefix(strings.TrimSpace(line[1:]), "*") {
+			return true
+		}
+	}
+	return false
+}
+
+//
+//
+///
