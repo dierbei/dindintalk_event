fda61fdc621c32b0dccbb6556f51df839fdb983f
和兑 <chenzhangchao@cai-inc.com>
2022-04-26 11:31:45 +0800

    feat: test

--- test.go
+++ test.go
@@ -13,8 +13,8 @@ import (
 
 func isAddComment(line string) bool {
 	if strings.HasPrefix(line, "+") {
-		if strings.HasPrefix(strings.TrimSpace(line[1:]), "//") ||
-			strings.HasPrefix(strings.TrimSpace(line[1:]), "/*") ||
+		if strings.HasPrefix(strings.TrimSpace(line[1:]), "//") || // asdasdasd
+			strings.HasPrefix(strings.TrimSpace(line[1:]), "/*") || // sadasdasd
 			strings.HasPrefix(strings.TrimSpace(line[1:]), "///") ||
 			strings.HasPrefix(strings.TrimSpace(line[1:]), "*") ||
 			strings.HasSuffix(strings.TrimSpace(line[1:]), "//") {
